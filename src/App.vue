f16cabf004511bdc3aa126b861ce8e9e8b53359c
ThubeesanKugathas <Thubeesan@outlook.com>
2022-01-31 12:46:40 +0100

    components creation + script imports

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-box />
+    <main-container />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import HeaderBox from './components/HeaderBox.vue'
+import MainContainer from './components/MainContainer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderBox,
+    MainContainer,
+  },
+  data() {
+    return {
+      dischiList: []
+    }
+  },
+  mounted() {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
+      this.dischiList = response.data;
+    });
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './style/app.scss';
+
 </style>
+
+
---
+++ src/components/MainContainer.vue
@@ -0,0 +1,30 @@
+<template>
+    <main>
+        <disc-box />
+    </main>
+</template>
+
+<script>
+import DiscBox from '../components/DiscBox.vue'
+
+export default {
+    components: {
+        DiscBox,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+// "success": true,
+    "response": [
+        {
+            "poster": "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+            "title": "New Jersey",
+            "author": "Bon Jovi",
+            "genre": "Rock",
+            "year": "1988"
+        },
+// 
