744a81a9bd635cc2d7dc9c72bf80e85d7070722c
ThubeesanKugathas <Thubeesan@outlook.com>
2022-02-01 15:15:07 +0100

    filter genre trough $emit in  FilterBox

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,47 @@
 <template>
   <div id="app">
     <header-box />
+    <filter-box @filtro="filterGenre" />
     <loader-div v-if="!flagLoader" />
-    <main-container v-else :dischiList="dischiList"/>
+    <main-container v-else :dischiList="filteredDischi"/>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import HeaderBox from './components/HeaderBox.vue'
-import MainContainer from './components/MainContainer.vue'
+import FilterBox from './components/FilterBox.vue'
 import LoaderDiv from './components/LoaderDiv.vue'
+import MainContainer from './components/MainContainer.vue'
 
 export default {
   name: 'App',
   components: {
     HeaderBox,
-    MainContainer,
+    FilterBox,
     LoaderDiv,
+    MainContainer,
   },
   data() {
     return {
       dischiList: [],
       flagLoader: false,
+      filteredDischi: [],
     }
   },
   mounted() {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
       this.dischiList = response.data.response;
+      this.filteredDischi = response.data.response;
       this.flagLoader = true;
     });
+  },
+  methods: {
+    filterGenre(value) {
+      this.filteredDischi = this.dischiList.filter((disco) => {
+        return disco.genre.includes(value);
+      });
+    },
   }
 }
 </script>
---
+++ src/components/FilterBox.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="d-flex justify-content-center">
+        <span>Filtro per genere:</span>
+        <select v-model="selectedValue">
+            <option disabled value="">Scegli un genere</option>
+            <option value="All">All</option>
+            <option value="Jazz">Jazz</option>
+            <option value="Metal">Metal</option>
+            <option value="Pop">Pop</option>
+            <option value="Rock">Rock</option>
+        </select>
+        <span @click="$emit('filtro', selectedValue)">Search</span>
+        <span>Reset</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selectedValue: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    div {
+        background-color: #1e2d3b;
+        text-align: center;
+        color: #fff;
+        padding-top: 50px;
+        font-size: 25px;
+        
+        select {
+            margin: 0 20px;
+        }
+
+        span {
+            margin: 0 20px;
+        }
+    }
+</style>
